78f3fa7d460d15a2d55be214a38eba76dc35ef15
brian d foy <bdfoy@cpan.org>
2007-01-15 22:55:10 +0000

    Import of BDFOY/p5-Palm-1.007 from CPAN.
    gitpan-cpan-distribution: p5-Palm
    gitpan-cpan-version:      1.007
    gitpan-cpan-path:         BDFOY/p5-Palm-1.007.tar.gz
    gitpan-cpan-author:       BDFOY
    gitpan-cpan-maturity:     released

--- lib/PDB.pm
+++ lib/PDB.pm
@@ -6,7 +6,7 @@
 #	You may distribute this file under the terms of the Artistic
 #	License, as specified in the README file.
 #
-# $Id: PDB.pm,v 1.29 2002/11/03 16:43:16 azummo Exp $
+# $Id: PDB.pm,v 1.36 2005/03/09 04:30:41 christophe Exp $
 
 # A Palm database file (either .pdb or .prc) has the following overall
 # structure:
@@ -25,7 +25,7 @@ package Palm::PDB;
 use vars qw( $VERSION %PDBHandlers %PRCHandlers );
 
 # One liner, to allow MakeMaker to work.
-$VERSION = do { my @r = (q$Revision: 1.29 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+$VERSION = do { my @r = (q$Revision: 1.36 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 =head1 NAME
 
@@ -69,6 +69,12 @@ individual chunks, then writes them to a file.
 
 =cut
 
+use constant dmRecordIDReservedRange => 1;
+					# The range of upper bits in the database's
+					# uniqueIDSeed from 0 to this number are
+					# reserved and not randomly picked when a
+					#database is created.
+
 my $EPOCH_1904 = 2082844800;		# Difference between Palm's
 					# epoch (Jan. 1, 1904) and
 					# Unix's epoch (Jan. 1, 1970),
@@ -77,6 +83,9 @@ my $HeaderLen = 32+2+2+(9*4);		# Size of database header
 my $RecIndexHeaderLen = 6;		# Size of record index header
 my $IndexRecLen = 8;			# Length of record index entry
 my $IndexRsrcLen = 10;			# Length of resource index entry
+
+# XXX Should we 'use constant' for the above vars?
+
 
 %PDBHandlers = ();			# Record handler map
 %PRCHandlers = ();			# Resource handler map
@@ -115,6 +124,26 @@ sub new
 	$self->{'uniqueIDseed'} = $params->{'uniqueIDseed'}	|| 0;
 
 	$self->{"2NULs"}	= "\0\0";
+
+	# This will be set when any elements of the object are changed
+	$self->{'dirty'} = 0;
+
+
+	# Calculate a proper uniqueIDseed if the user has not provided
+	# a correct one.
+	if ($self->{'uniqueIDseed'} <= ((dmRecordIDReservedRange + 1) << 12))
+	{
+		my $uniqueIDseed = 0;
+
+		do
+		{
+			$uniqueIDseed = int(rand(0x0FFF));
+
+		} while ($uniqueIDseed <= dmRecordIDReservedRange);
+
+		$self->{'uniqueIDseed'} = $uniqueIDseed << 12;
+		$self->{'uniqueIDseed'} &= 0x00FFF000;		# Isolate the upper 12 seed bits.
+	}
 
 	bless $self, $class;
 	return $self;
@@ -268,11 +297,14 @@ sub RegisterPRCHandlers
 
 =head2 Load
 
-  $pdb->Load("filename");
+  $pdb->Load($filename);
 
-Reads the file F<filename>, parses it, reblesses $pdb to the
+Reads the file C<$filename>, parses it, reblesses $pdb to the
 appropriate class, and invokes appropriate methods to parse the
 application-specific parts of the database (see L</HELPER CLASSES>).
+
+C<$filename> may also be an open file handle (as long as it's
+seekable). This allows for manipulating databases in memory structures.
 
 Load() uses the I<typespec>s given to RegisterPDBHandlers() and
 RegisterPRCHandlers() when deciding how to rebless $pdb. For record
@@ -420,6 +452,48 @@ format given above.
 =cut
 #'
 
+# _open
+sub _open
+{
+	my($self, $mode, $fname) = @_;
+	
+	my $handle;
+	
+	if (ref($fname))
+	{
+		# Already a filehandle
+		if (ref($fname) eq 'GLOB' 
+		    or UNIVERSAL::isa($fname,"IO::Seekable"))
+		{
+			$handle = $fname;
+		}
+		# Probably a reference to a SCALAR
+		else
+		{
+			unless (eval 'open $handle, $mode, $fname')
+			{
+				if ($@ ne '')
+				{
+				    die "Open of \"$fname\" unsupported: $@\n";
+				}
+				else
+				{
+				    die "Can't open \"$fname\": $!\n";
+				}
+			}
+		}
+	}
+	else
+	{
+		# Before 5.6.0 "autovivified file handles" don't exist
+		eval 'use IO::File; $handle = new IO::File' if $] < 5.006;
+		open $handle, "$mode $fname" 
+		    or die "Can't open \"$fname\": $!\n";
+	}
+
+	return $handle;
+}
+
 # Load
 sub Load
 {
@@ -427,14 +501,15 @@ sub Load
 	my $fname = shift;		# Filename to read from
 	my $buf;			# Buffer into which to read stuff
 
-	# Open database file
-	open PDB, "< $fname" or die "Can't open \"$fname\": $!\n";
-	binmode PDB;			# Read as binary file under MS-DOS
+	my $handle = $self->_open('<', $fname);
+	return undef unless defined $handle;
+
+	binmode $handle;	# Read as binary file under MS-DOS
 
 	# Get the size of the file. It'll be useful later
-	seek PDB, 0, 2;		# 2 == SEEK_END. Seek to the end.
-	$self->{_size} = tell PDB;
-	seek PDB, 0, 0;		# 0 == SEEK_START. Rewind to the beginning.
+	seek $handle, 0, 2;	# 2 == SEEK_END. Seek to the end.
+	$self->{_size} = tell $handle;
+	seek $handle, 0, 0;	# 0 == SEEK_START. Rewind to the beginning.
 
 	# Read header
 	my $name;
@@ -450,13 +525,16 @@ sub Load
 	my $creator;
 	my $uniqueIDseed;
 
-	read PDB, $buf, $HeaderLen;	# Read the PDB header
+	read $handle, $buf, $HeaderLen;	# Read the PDB header
 
 	# Split header into its component fields
 	($name, $attributes, $version, $ctime, $mtime, $baktime,
 	$modnum, $appinfo_offset, $sort_offset, $type, $creator,
 	$uniqueIDseed) =
 		unpack "a32 n n N N N N N N a4 a4 N", $buf;
+
+	# database names must include a terminating NUL.
+	die "bogus database name! is this really a PalmOS file?" unless $name =~ /.+\0/;
 
 	($self->{name} = $name) =~ s/\0.*$//;
 	$self->{attributes}{resource} = 1 if $attributes & 0x0001;
@@ -497,6 +575,17 @@ sub Load
 	$self->{creator} = $creator;
 	$self->{uniqueIDseed} = $uniqueIDseed;
 
+	# XXX strictly speaking, ctime/mtime/baktime values before 1990 are quite
+	# unlikely. Palm was founded in 1992, so even allowing for some prototypes.
+	# This is another way one could detect bogus databases.
+
+	if( $self->{_appinfo_offset} > $self->{_size} ) {
+		die "AppInfo block offset beyond end of file!";
+	}
+	if( $self->{_sort_offset} > $self->{_size} ) {
+		die "Sort block offset beyond end of file!";
+	}
+
 	# Rebless this PDB object, depending on its type and/or
 	# creator. This allows us to magically invoke the proper
 	# &Parse*() function on the various parts of the database.
@@ -534,7 +623,7 @@ sub Load
 
 	## Read record/resource index
 	# Read index header
-	read PDB, $buf, $RecIndexHeaderLen;
+	read $handle, $buf, $RecIndexHeaderLen;
 
 	my $next_index;
 	my $numrecs;
@@ -545,9 +634,9 @@ sub Load
 	# Read the index itself
 	if ($self->{attributes}{resource} || $self->{'attributes'}{'ResDB'})
 	{
-		&_load_rsrc_index($self, \*PDB);
+		&_load_rsrc_index($self, $handle);
 	} else {
-		&_load_rec_index($self, \*PDB);
+		&_load_rec_index($self, $handle);
 	}
 
 	# Read the two NUL bytes
@@ -555,27 +644,27 @@ sub Load
 	# spec. The Right Thing to do is to ignore them, and use the
 	# specified or calculated offsets, if they're sane. Sane ==
 	# appears later than the current position.
-#	read PDB, $buf, 2;
+#	read $handle, $buf, 2;
 #	$self->{"2NULs"} = $buf;
 
 	# Read AppInfo block, if it exists
 	if ($self->{_appinfo_offset} != 0)
 	{
-		&_load_appinfo_block($self, \*PDB);
+		&_load_appinfo_block($self, $handle);
 	}
 
 	# Read sort block, if it exists
 	if ($self->{_sort_offset} != 0)
 	{
-		&_load_sort_block($self, \*PDB);
+		&_load_sort_block($self, $handle);
 	}
 
 	# Read record/resource list
 	if ($self->{attributes}{resource} || $self->{'attributes'}{'ResDB'})
 	{
-		&_load_resources($self, \*PDB);
+		&_load_resources($self, $handle);
 	} else {
-		&_load_records($self, \*PDB);
+		&_load_records($self, $handle);
 	}
 
 	# These keys were needed for parsing the file, but are not
@@ -586,7 +675,9 @@ sub Load
 	delete $self->{_sort_offset};
 	delete $self->{_size};
 
-	close PDB;
+	$self->{'dirty'} = 0;
+
+	return $self;
 }
 
 # _load_rec_index
@@ -714,7 +805,7 @@ sub _load_appinfo_block
 	# Seek to the right place, if necessary
 	if (tell($fh) != $pdb->{_appinfo_offset})
 	{
-		seek PDB, $pdb->{_appinfo_offset}, 0;
+		seek $fh, $pdb->{_appinfo_offset}, 0;
 	}
 
 	# There's nothing that explicitly gives the size of the
@@ -769,7 +860,7 @@ sub _load_sort_block
 	# Seek to the right place, if necessary
 	if (tell($fh) != $pdb->{_sort_offset})
 	{
-		seek PDB, $pdb->{_sort_offset}, 0;
+		seek $fh, $pdb->{_sort_offset}, 0;
 	}
 
 	# There's nothing that explicitly gives the size of the sort
@@ -825,11 +916,15 @@ sub _load_records
 				" but it's at ",
 				sprintf("[0x%08x]", tell($fh)), "\n";
 		}
+		
+		if( $pdb->{_index}[$i]{offset} > $pdb->{_size} ) {
+			die "corruption: Record $i beyond end of database!";
+		}
 
 		# Seek to the right place, if necessary
 		if (tell($fh) != $pdb->{_index}[$i]{offset})
 		{
-			seek PDB, $pdb->{_index}[$i]{offset}, 0;
+			seek $fh, $pdb->{_index}[$i]{offset}, 0;
 		}
 
 		# Compute the length of the record: the last record
@@ -888,10 +983,14 @@ sub _load_resources
 				sprintf("0x%08x", tell($fh)), "\n";
 		}
 
+		if( $pdb->{_index}[$i]{offset} > $pdb->{_size} ) {
+			die "corruption: Resource $i beyond end of database!";
+		}
+
 		# Seek to the right place, if necessary
 		if (tell($fh) != $pdb->{_index}[$i]{offset})
 		{
-			seek PDB, $pdb->{_index}[$i]{offset}, 0;
+			seek $fh, $pdb->{_index}[$i]{offset}, 0;
 		}
 
 		# Compute the length of the resource: the last
@@ -927,11 +1026,14 @@ sub _load_resources
 
 =head2 Write
 
-  $pdb->Write("filename");
+  $pdb->Write($filename);
 
 Invokes methods in helper classes to get the application-specific
 parts of the database, then writes the database to the file
-I<filename>.
+C<$filename>.
+
+C<$filename> may also be an open file handle (as long as it's
+seekable). This allows for manipulating databases in memory structures.
 
 Write() uses the following helper methods:
 
@@ -965,9 +1067,14 @@ sub Write
 	my $fname = shift;		# Output file name
 	my @record_data;
 
+	die "Can't write a database with no name\n"
+		unless $self->{name} ne "";
+
+	my $handle = $self->_open('>', $fname);
+	return undef unless defined $handle;
+
 	# Open file
-	open OFILE, "> $fname" or die "Can't write to \"$fname\": $!\n";
-	binmode OFILE;			# Write as binary file under MS-DOS
+	binmode $handle;	# Write as binary file under MS-DOS
 
 	# Get AppInfo block
 	my $appinfo_block = $self->PackAppInfoBlock;
@@ -1122,13 +1229,13 @@ sub Write
 		$self->{uniqueIDseed};
 		;
 
-	print OFILE "$header";
+	print $handle "$header";
 
 	# Write index header
 	my $index_header;
 
 	$index_header = pack "N n", 0, ($#record_data+1);
-	print OFILE "$index_header";
+	print $handle "$index_header";
 
 	# Write index
 	my $rec_offset;		# Offset of next record/resource
@@ -1162,7 +1269,7 @@ sub Write
 				$type,
 				$id,
 				$rec_offset;
-			print OFILE "$index_data";
+			print $handle "$index_data";
 
 			$rec_offset += length($data);
 		}
@@ -1192,7 +1299,7 @@ sub Write
 				($id >> 16) & 0xff,
 				($id >> 8) & 0xff,
 				$id & 0xff;
-			print OFILE "$index_data";
+			print $handle "$index_data";
 
 			$rec_offset += length($data);
 		}
@@ -1201,16 +1308,16 @@ sub Write
 	# Write the two NULs
 	if (length($self->{"2NULs"}) == 2)
 	{
-		print OFILE $self->{"2NULs"};
+		print $handle $self->{"2NULs"};
 	} else {
-		print OFILE "\0\0";
+		print $handle "\0\0";
 	}
 
 	# Write AppInfo block
-	print OFILE $appinfo_block unless $appinfo_offset == 0;
+	print $handle $appinfo_block unless $appinfo_offset == 0;
 
 	# Write sort block
-	print OFILE $sort_block unless $sort_offset == 0;
+	print $handle $sort_block unless $sort_offset == 0;
 
 	# Write record/resource list
 	my $record;
@@ -1231,10 +1338,10 @@ sub Write
 
 			($attributes, $id, $data) = @{$record};
 		}
-		print OFILE $data;
+		print $handle $data;
 	}
 
-	close OFILE;
+	return $self;
 }
 
 =head2 new_Record
@@ -1280,6 +1387,44 @@ sub new_Record
 	return $retval;
 }
 
+=head2 is_Dirty
+
+  $pdb->Write( $fname ) if $pdb->is_Dirty();
+
+Returns non-zero if any of the in-memory elements of the database have
+been changed. This includes changes via function calls (any call that
+changes the C<$pdb>'s "last modification" time) as well as testing the
+"dirty" status of attributes where possible (i.e. AppInfo, records,
+but not resource entries).
+
+=cut
+#'
+
+sub is_Dirty
+{
+	my $self = shift;
+
+	# try the quick and easy tests first
+	return 1 if $self->{'dirty'};
+	return 1 if $self->{'attributes'}{'AppInfoDirty'};
+	return 1 if $self->{'attributes'}{'AppInfo dirty'};
+
+	# okay, check the records. Note that resource entries appear to
+	# have no dirty flags for us to use.
+	if (!$self->{attributes}{resource} and !$self->{'attributes'}{'ResDB'})
+	{
+		my $record;
+
+		foreach $record (@{$self->{records}})
+		{
+			return 1 if $record->{'attributes'}{'Dirty'};
+			return 1 if $record->{'attributes'}{'dirty'};
+		}
+	}
+
+	return 0;
+}
+
 =head2 append_Record
 
   $record  = $pdb->append_Record;
@@ -1311,12 +1456,24 @@ sub append_Record
 		# No arguments given. Create a new record.
 		my $record = $self->new_Record;
 
+		# Validate the unique ID.
+		$self->_setUniqueID($record)
+			if $record->{'id'} eq 0;
+
 		push @{$self->{records}}, $record;
 
 		# Update the "last modification time".
 		$self->{mtime} = time;
+		$self->{dirty} = 1;
 
 		return $record;
+	}
+
+	# Validate the unique IDs.
+	foreach my $record (@_)
+	{
+		$self->_setUniqueID($record)
+			if $record->{'id'} eq 0;
 	}
 
 	# At least one argument was given. Append all of the arguments
@@ -1325,11 +1482,32 @@ sub append_Record
 
 	# Update the "last modification time".
 	$self->{mtime} = time;
+	$self->{'dirty'} = 1;
 
 	return $_[0];
 }
 
-=head2
+sub _setUniqueID
+{
+	my($self, $record) = @_;
+
+	# Bump the seed to prevent a uniqueIDseed of 0 which represents
+	# an unassigned uniqueID.
+	# XXX IMHO this just couldn't happen given the way the seed it's
+	# generated. But if Palm OS goes this way maybe it's better to do
+	# the same.
+
+	$self->{'uniqueIDseed'}++;
+
+	# Check for wrap around. Remember that an uniqueID is made of only 24 bits.
+	$self->{'uniqueIDseed'} = (dmRecordIDReservedRange + 1) << 12
+		if ($self->{'uniqueIDseed'} & 0xFF000000);
+
+	# Copy the seed into the new record.
+	$record->{'id'} = $self->{'uniqueIDseed'};
+}
+
+=head2 new_Resource
 
   $resource = Palm::PDB->new_Resource();
   $resource = new_Resource Palm::PDB;
@@ -1390,6 +1568,7 @@ sub append_Resource
 
 		# Update the "last modification time".
 		$self->{mtime} = time;
+		$self->{'dirty'} = 1;
 
 		return $resource;
 	}
@@ -1400,6 +1579,7 @@ sub append_Resource
 
 	# Update the "last modification time".
 	$self->{mtime} = time;
+	$self->{'dirty'} = 1;
 
 	return $_[0];
 }
@@ -1473,6 +1653,50 @@ sub delete_Record
 
 	# Update the "last modification time".
 	$self->{mtime} = time;
+	$self->{'dirty'} = 1;
+}
+
+=head2 remove_Record
+
+	for (@{$pdb->{'records'}})
+	{
+		$pdb->remove_Record( $_ ) if $_->{attributes}{deleted};
+	}
+
+Removes C<$record> from the database. This differs from C<delete_Record>
+in that it's an actual deletion rather than just setting a flag.
+
+This method updates $pdb's "last modification" time.
+
+=cut
+#'
+
+sub remove_Record($$)
+{
+	my $self = shift;
+	my $record = shift;
+
+	for (my $i = 0; $i <= $#{$self->{records}}; $i ++)
+	{
+		if ($self->{records}->[$i] == $record)
+		{
+			# make a copy of the records array. This is really necessary
+			# because there's frequently something using the records reference
+			# for iteration purposes (like the doc example) and we can't
+			# just start splicing that apart (tried, failed).
+			# So we have to make a new copy. This does, unfortunately,
+			# make remove_Record() more expensive that you'd expect.
+			$self->{records} = [ @{$self->{records}} ];
+
+			# remove the record index.
+			splice @{$self->{records}}, $i, 1;
+
+			$self->{mtime} = time;
+			$self->{'dirty'} = 1;
+
+			last;
+		}
+	}
 }
 
 1;
